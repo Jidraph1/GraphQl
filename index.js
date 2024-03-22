bcb13fc995ca32466a6db6a6ab6da1a69145fefe
Jidraph1 <jidraphkimachia1@gmail.com>
2024-03-22 15:28:01 +0300

    Complete Apollo GraphQl trial

--- index.js
+++ index.js
@@ -12,18 +12,61 @@ const resolvers = {
     games(){
       return db.games
     },
+    game(_, args){
+      return db.games.find((game) => game.id === args.id)
+    },
     reviews(){
       return db.reviews
     },
+    review(_, args){
+      return db.reviews.find((review) => review.id === args.id)
+    },
     authors(){
       return db.authors
+    },
+    author(_, args){
+      return db.authors.find((author) => author.id === args.id)
+    }
+  },
+  Game: {
+    reviews(parent){
+      return db.reviews.filter((r) => r.game_id === parent.id)
+    }
+    },
+  Author: {
+    reviews(parent){
+      return db.reviews.filter((a) => a.author_id === parent.id)
+    }
+    },
+  Mutation: {
+    deleteGame(_, args){
+      db.games = db.games.filter((g) => g.id !== args.id)
+      return db.games
+    },
+    addGame(_, args){
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random()* 1000).toString()
+      }
+      db.games.push(game)
+      return game
+    },
+    updateGame(_, args){
+      db.games = db.games.map((g) => {
+        if(g.id === args.id){
+          return {...g, ...args.input}
+        }
+        return g
+      })
+
+      return db.games.find((g) => g.id === args.id)
     }
   }
 }
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers,
+    resolvers
   });
   
   
--- schema.js
+++ schema.js
@@ -4,25 +4,48 @@ type Game {
     id: ID!
     title: String!
     platform: [String!]!
+    reviews: [Review!]
+
 }
 
 type Review {
     id: ID!
     rating: Int!
     content: String!
+    author: Author!
+    game: Game!
 }
 
 type Author {
     id: ID!
     name: String!
     verified: Boolean!
+    reviews: [Review!]
+
 }
 
 type Query {
     reviews: [Review]
+    review(id: ID!): Review
     games: [Game]
+    game(id: ID!): Game
     authors: [Author]
+    author(id: ID!): Author
 }
 
+type Mutation {
+    addGame(input: AddGameInput): Game
+    updateGame(id: ID!, input: EditGameInput!): Game
+    deleteGame(id: ID!): [Game]
 
+}
+input AddGameInput { 
+    title: String!,
+    platform: [String!]!
+}
+
+input EditGameInput {
+    title: String,
+    platform: [String!]
+}
 `
